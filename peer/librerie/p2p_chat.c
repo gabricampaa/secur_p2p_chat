d6fb6e30c3669424e1e2f31aa09395e9d7778116
gabricampaa <135340970+gabricampaa@users.noreply.github.com>
2024-11-07 14:43:10 +0100

    Update p2p_chat.c

--- peer/librerie/p2p_chat.c
+++ peer/librerie/p2p_chat.c
@@ -8,109 +8,96 @@
 #include <pthread.h>
 
 #include "miaLibVarie.h"
-#include  "RSACommLib.h"
+#include "RSACommLib.h"
 
-//gcc -o main_bb DEF_p2p_Chat_simo_cript.c librerie/RSACommLib.c librerie/miaLibVarie.c  -lssl -lcrypto -w -lcurl  -g
+#define PORT 51810 // The port on which our connections happen
 
-// gcc -o main_bb main.c librerie/RSACommLib.c librerie/miaLibVarie.c  -lssl -lcrypto -w -lcurl  -g
-//#define LISTENING_PORT_RECIVING_FILES 51811
-//#define PORT_FOR_SENDING_FILES 51812
-
-
-int PORT = 51810;
+char pathToHostPrivateKey[256];
+pthread_mutex_t buffer_mutex;
+pthread_cond_t message_cond;
 
 void sending(char *TargetIp, char *pathToPeerPubKey);
 void receiving(int server_fd);
 void *receive_thread(void *server_fd);
 
+int secureP2Pchat_simone(char* TargetIp, char* peer_s_pubKeyPath, char* pathToHostPrivKey) {
+    char cwd[1024];
+    getcwd(cwd, sizeof(cwd));
+    char *workingDirectory = cwd;
+    const char *fileChiave = concatenateStrings(workingDirectory, ".secrets/public_key.pem");
 
-//char *pathToHostPrivateKey= NULL;
-char *pathToHostPrivateKey[256];
+    snprintf(pathToHostPrivateKey, sizeof(pathToHostPrivateKey), "%s", pathToHostPrivKey);
 
+    char* publicIP = getHostIp(); // This so you can tell this to your buddy
+    printf("Public IPv4 address of this machine: %s\n\n", publicIP);
+    free(publicIP); // Free memory allocated for publicIP
 
-int secureP2Pchat_simone(char* TargetIp, char* peer_s_pubKeyPath, char* pathToHostPrivKey)
-{
-
-        char cwd[1024];
-        getcwd(cwd, sizeof(cwd));
-    	char *workingDirectory = cwd;
-        const char *fileChiave = concatenateStrings(workingDirectory, ".secrets/public_key.pem");
-
-        //pathToHostPrivateKey = pathToHostPrivKey;
-        snprintf(pathToHostPrivateKey, sizeof(pathToHostPrivateKey), "%s", pathToHostPrivKey);
-
-        char name[25];
-
-        char* publicIP = getHostIp();
-        printf("Public IPv4 address of this machine: %s\n\n", publicIP);
-        free(publicIP); // Free memory allocated for publicIP
-
-
-
-    int server_fd, new_socket, valread;
+    int server_fd;
     struct sockaddr_in address;
-    int k = 0;
 
-    // Creazione del socket file descriptor
-    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == -1)
-    {
+    // Creating socket file descriptor
+    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == -1) {
         perror("socket failed");
         exit(EXIT_FAILURE);
     }
-    // Attach del socket alla porta
 
+    // Attach socket to the port
     address.sin_family = AF_INET;
     address.sin_addr.s_addr = INADDR_ANY;
     address.sin_port = htons(PORT);
 
-    // Stampa dell'indirizzo IP e della porta del server
+    // Print server IP and port
     printf("IP address is: %s\n", inet_ntoa(address.sin_addr));
     printf("Port is: %d\n", (int)ntohs(address.sin_port));
 
-    if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0)
-    {
+    if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0) {
         perror("bind failed");
         exit(EXIT_FAILURE);
     }
-    if (listen(server_fd, 5) < 0)
-    {
+    if (listen(server_fd, 5) < 0) {
         perror("listen");
         exit(EXIT_FAILURE);
     }
-    int ch;
+
+    // Initialize mutex and condition variable
+    pthread_mutex_init(&buffer_mutex, NULL);
+    pthread_cond_init(&message_cond, NULL);
+
     pthread_t tid;
-    pthread_create(&tid, NULL, &receive_thread, &server_fd); // Creazione del thread per continuare a ricevere i messaggi in tempo reale
+    pthread_create(&tid, NULL, &receive_thread, &server_fd); // Create thread to continuously receive messages
+
     printf("\n*****At any point in time press the following:*****\n1. Send message\n0. Quit\n");
-    printf("\nEnter choice:");
-    do
-    {
+
+    int ch;
+    do {
+        printf("\nEnter choice: ");
         scanf("%d", &ch);
-        switch (ch)
-        {
-        case 1:
-            sending(TargetIp,peer_s_pubKeyPath);
-            break;
-        case 0:
-            printf("\nLeaving\n");
-            break;
-        default:
-            printf("\nWrong choice\n");
+        getchar(); // consume newline character after scanf
+        switch (ch) {
+            case 1:
+                sending(TargetIp, peer_s_pubKeyPath);
+                break;
+            case 0:
+                printf("\nLeaving\n");
+                break;
+            default:
+                printf("\nWrong choice\n");
         }
     } while (ch);
 
     close(server_fd);
+    pthread_mutex_destroy(&buffer_mutex);
+    pthread_cond_destroy(&message_cond);
 
     return 0;
 }
 
 void sending(char* TargetIp, char *pathToPeerPubKey) {
     char buffer[2000] = {0};
-    int PORT_server = PORT;
-    char dummy;
-
-    int sock = 0;
-    struct sockaddr_in serv_addr;
     char hello[1024] = {0};
+    int sock = 0;
+
+    struct sockaddr_in serv_addr;
 
     if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
         printf("\n Socket creation error \n");
@@ -119,23 +106,21 @@ void sending(char* TargetIp, char *pathToPeerPubKey) {
 
     serv_addr.sin_family = AF_INET;
     serv_addr.sin_addr.s_addr = inet_addr(TargetIp);
-    serv_addr.sin_port = htons(PORT_server);
+    serv_addr.sin_port = htons(PORT);
 
     if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) {
         printf("\nConnection Failed \n");
         return;
     }
 
-    fflush(stdout); // Aggiungi questo per forzare l'output immediato
+    fflush(stdout); // Ensure immediate output
 
     printf("Enter your message: ");
-    //scanf("%c", &dummy); // Clear the newline character from the buffer
-
     if (fgets(hello, sizeof(hello), stdin) == NULL) {
-            perror("Error reading input");
-            close(sock);
-            return;
-        }
+        perror("Error reading input");
+        close(sock);
+        return;
+    }
 
     char ciphertext[300];
     int ciphertext_len = RSACommLib_Encrypt(pathToPeerPubKey, hello, ciphertext);
@@ -145,30 +130,23 @@ void sending(char* TargetIp, char *pathToPeerPubKey) {
         return;
     }
 
-
-    send(sock,ciphertext,strlen(ciphertext), 0);
+    send(sock, ciphertext, strlen(ciphertext), 0);
     printf("\nMessage sent.\n");
 
     close(sock);
-    fflush(stdout); // Aggiungi questo per forzare l'output immediato
-
+    fflush(stdout); // Ensure immediate output
 }
 
-
-
-//Calling receiving every 2 seconds
-void *receive_thread(void *server_fd)
-{
+void *receive_thread(void *server_fd) {
     int s_fd = *((int *)server_fd);
-    while (1)
-    {
+    while (1) {
         sleep(2);
         receiving(s_fd);
     }
 }
 
-
 void receiving(int server_fd) {
+    printf("\n");
     struct sockaddr_in address;
     int addrlen = sizeof(address);
     int client_socket;
@@ -177,8 +155,7 @@ void receiving(int server_fd) {
         perror("accept");
         exit(EXIT_FAILURE);
     }
-    fflush(stdout); // Aggiungi questo per forzare l'output immediato
-
+    fflush(stdout); // Ensure immediate output
 
     char buffer[2048] = {0};
     char testoDecifato[2048];
@@ -191,18 +168,8 @@ void receiving(int server_fd) {
     }
     RSACommLib_Decrypt(pathToHostPrivateKey, buffer, testoDecifato);
 
-    printf("\n\n\n-----START RECIVING MESSASGEE-----\n\n%s \n\n\n%s\n\n----END----", buffer, testoDecifato);
-    int buf_len = strlen(buffer) / 2;
+    printf("\n\n\n-----START RECEIVING MESSAGE-----\n\n%s\n\n----END----", testoDecifato);
 
-    char *decrDef = malloc(2048); // Assicurati di allocare memoria per decrDef
-    if (decrDef == NULL) {
-        fprintf(stderr, "Error allocating memory for decrypted message.\n");
-        close(client_socket);
-        return;
-    }
-
-    free(decrDef); // Libera la memoria allocata per decrDef
     close(client_socket);
-    fflush(stdout); // Aggiungi questo per forzare l'output immediato
-
+    fflush(stdout); // Ensure immediate output
 }
