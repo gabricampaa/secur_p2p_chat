5642fbd2ce75b72ee5dd7e1f45422609d5033f34
gabricampaa <135340970+gabricampaa@users.noreply.github.com>
2024-11-16 12:10:53 +0100

    Update RSACommLib.h

--- peer/librerie/RSACommLib.h
+++ peer/librerie/RSACommLib.h
@@ -3,9 +3,14 @@
 
 int RSACommLib_Init(int key_size, const char *path_to_save_priv_key, const char *path_to_save_pub_key);
 
-int RSACommLib_Encrypt( const char *public_key_file, const char *input_buffer, char* output_buffer);
+//int RSACommLib_Encrypt( const char *public_key_file, const char *input_buffer, char* output_buffer);
 
-int RSACommLib_Decrypt(const char *private_key_file, const char *input_buffer, char* output_buffer);
+//int RSACommLib_Decrypt(const char *private_key_file, const char *input_buffer, char* output_buffer);
+
+
+int RSACommLib_Encrypt(const char *public_key_file, const char *input_buffer, char *output_buffer, int output_size);
+
+int RSACommLib_Decrypt(const char *private_key_file, const char *input_buffer, char *output_buffer, int output_size);
                                                  
 #endif
 
